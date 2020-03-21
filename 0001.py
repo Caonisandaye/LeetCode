4ab314dbf3a73075ff0049cb63ccc3e412ecc87a
Jianze Zhou <jnz2@georgetown.edu>
2020-03-21 02:27:32 -0400

    Update 0001.py

--- 0001.py
+++ 0001.py
@@ -1,4 +1,14 @@
-"""
-1. Two Sum
-"""
-#Two Sum
+# 1. Two Sum
+
+# Solution 1: Brute Force
+# Loop through all the two number combination is the list to see if it matches the sum
+# Time complexity: O(N). Space complexity: O(1).
+
+class Solution1(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
