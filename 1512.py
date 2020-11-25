d9241a2ed72cc0e275db74380aeb1a1436cfebb5
Jianze Zhou <jnz2@georgetown.edu>
2020-11-25 13:08:33 -0500

    Update 1512.py

--- 1512.py
+++ 1512.py
@@ -18,4 +18,4 @@ class Solution(object):
         
 #Result Runtime: 16 ms / 92.18%; Memory Usage: 13.4 MB / 70.30%
 
-#Instruction: Math 101 -- n identical numbers means n * (n - 1) / 2 good pairs.
+#Instruction: Math 101 -- n identical numbers means n * (n - 1) / 2 good pairs. Might be improved with counter(), but I am too lazy.
