16d17b1ab832b136647cc2c2db154b61d415fe2c
Jianze Zhou <jnz2@georgetown.edu>
2020-11-24 21:01:06 -0500

    Create 1431.py

---
+++ 1431.py
@@ -0,0 +1,17 @@
+##LC 1431. Kids With the Greatest Number of Candies
+
+#Solution
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        grt_candies = max(candies)
+        
+        return [(cndy + extraCandies) >= grt_candies for cndy in candies]
+
+#Result Runtime: 28 ms / 41.98%; Memory Usage: 13.3 MB / 69.89%
+
+#Instruction: Simple for loop.
