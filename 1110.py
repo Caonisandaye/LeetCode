0bea0c0a58e8479bac51e172ef870e8030489c43
Jianze Zhou <jnz2@georgetown.edu>
2020-11-24 20:38:51 -0500

    Create 1110.py

---
+++ 1110.py
@@ -0,0 +1,12 @@
+##LC 1119. Remove Vowels from a String
+
+#Solution
+class Solution(object):
+    def removeVowels(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        return ''.join([l for l in S if l not in 'aeiou'])
+
+#Result Runtime: 16 ms/73.65 Memory Usage: 13.6 MB
