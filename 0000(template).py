8f899ada7357d2f6822fd96c83627b3969607bc5
Jianze Zhou <jnz2@georgetown.edu>
2020-11-25 19:10:53 -0500

    Update and rename 0000(sample).py to 0000(template).py

---
+++ 0000(template).py
@@ -0,0 +1,13 @@
+##LC 000. py file template
+
+#Solution
+class Solution(object):
+    def sampleMethod(self, arg):
+        """
+        :type arg: int
+        :rtype: int
+        """
+                
+#Result Runtime: __ ms / __%; Memory Usage: __ MB / __%
+
+#Instruction: XXXXXX
