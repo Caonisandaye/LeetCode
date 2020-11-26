f1b08cb75ac9d4d790056bc011204f05aa3b9e84
Jianze Zhou <jnz2@georgetown.edu>
2020-11-25 19:18:44 -0500

    Create 1528.py

---
+++ 1528.py
@@ -0,0 +1,20 @@
+##LC 1528. Shuffle String
+
+#Solution
+class Solution(object):
+    def restoreString(self, s, indices):
+        """
+        :type s: str
+        :type indices: List[int]
+        :rtype: str
+        """
+        L = len(s)
+        sfld_str = [''] * L
+        for i in range(L):
+            sfld_str[indices[i]] = s[i]
+        
+        return ''.join(sfld_str)
+                
+#Result Runtime: 40 ms / 60.51%; Memory Usage: 13.6 MB / 37.55%
+
+#Instruction: Understand the right way to shuffle.
