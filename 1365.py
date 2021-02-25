2ed92254488034aefb4c7d34c9580cc39e51cd80
Jianze Zhou <jnz2@georgetown.edu>
2021-02-25 14:22:32 -0500

    Update 1365.py

--- 1365.py
+++ 1365.py
@@ -7,10 +7,12 @@ class Solution(object):
         :rtype: List[int]
         """
         ct = [0 for i in range(101)]
+        
         for n in nums:
             ct[n] += 1
         for i in range(1, 101):
             ct[i] += ct[i-1]
+        
         ct = [0] + ct
         
         return [ct[n] for n in nums]
