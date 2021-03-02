e6771478cc418f8f2e9b86dcf4a6a2b69d1122f9
Jianze Zhou <jnz2@georgetown.edu>
2021-03-01 23:38:30 -0500

    Create 1688.py

---
+++ 1688.py
@@ -0,0 +1,14 @@
+##LC 1688. Count of Matches in Tournament
+
+#Solution
+class Solution(object):
+    def numberOfMatches(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n-1
+                
+#Result Runtime: 12 ms / 94.00%; Memory Usage: 13.4 MB / 37.70%
+
+#Instruction: simple math, one game eliminates one team
