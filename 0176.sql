b397b2d144aa6afe01c268fd02c68b1775d38dfe
Jianze Zhou <jnz2@georgetown.edu>
2021-09-18 14:10:39 -0400

    Create 0176.sql

---
+++ 0176.sql
@@ -0,0 +1,12 @@
+##LC 176. Second Highest Salary
+
+#Solution: Write your MySQL query statement below
+SELECT IFNULL(
+(SELECT DISTINCT Salary  FROM
+(SELECT Salary, DENSE_RANK() OVER (ORDER BY Salary DESC) rk
+FROM Employee) A
+WHERE rk = 2), NULL) SecondHighestSalary
+                
+#Result Runtime: 182 ms / 68.47%; Memory Usage: 0 MB / 100.00%
+
+#Instruction: use DENSE_RANK() to query the rank we want, LIMIT + OFFSET might give a better solution, use IFNULL to layout empty table as NULL
