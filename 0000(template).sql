18622b549b8b6fbf6ea4da1fb1be87c6e7eb33cb
Jianze Zhou <jnz2@georgetown.edu>
2021-09-18 14:07:54 -0400

    Update 0000(template).sql

--- 0000(template).sql
+++ 0000(template).sql
@@ -1,7 +1,7 @@
 ##LC 0. sql file template
 
 #Solution: Write your MySQL query statement below
-
+SELECT sample_column FROM sample_table
                 
 #Result Runtime: __ ms / __%; Memory Usage: __ MB / __%
 
