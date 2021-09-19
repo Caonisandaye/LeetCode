40cb66e1f0882e86d2e7c5853147169467f10f1e
Jianze Zhou <jnz2@georgetown.edu>
2021-09-19 15:09:10 -0400

    Create 0197.sql

---
+++ 0197.sql
@@ -0,0 +1,11 @@
+##LC 197. Rising Temperature
+
+#Solution
+SELECT A.id
+FROM Weather A
+JOIN Weather B 
+ON DATEDIFF(A.recordDate, B.recordDate) = 1 AND A.Temperature > B.Temperature
+
+#Result Runtime: 488 ms / 34.19%; Memory Usage: 0 MB / 100.00%
+
+#Instruction: DATEDIFF() and TIMESTAMPDIFF() are both date calculator in MySql
