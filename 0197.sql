fe4cbaacfc8ad4286e939b3512a3d2aa6eacd071
Jianze Zhou <jnz2@georgetown.edu>
2021-09-19 15:11:05 -0400

    Update 0197.sql

--- 0197.sql
+++ 0197.sql
@@ -8,4 +8,4 @@ ON DATEDIFF(A.recordDate, B.recordDate) = 1 AND A.Temperature > B.Temperature
 
 #Result Runtime: 488 ms / 34.19%; Memory Usage: 0 MB / 100.00%
 
-#Instruction: DATEDIFF() and TIMESTAMPDIFF() are both date calculator in MySql
+#Instruction: DATEDIFF() and TIMESTAMPDIFF() are both date calculator in MySQL, note they are taking date parameters in different order
