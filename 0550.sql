5424c120bd4116c0ad218a0baa707e37225d6474
Jianze Zhou <jnz2@georgetown.edu>
2021-09-19 21:16:01 -0400

    Create 0550.sql

---
+++ 0550.sql
@@ -0,0 +1,8 @@
+##LC 550. Game Play Analysis IV
+
+#Solution
+SELECT sample_column FROM sample_table
+                
+#Result Runtime: 427 ms / 95.41%; Memory Usage: 0 MB / 100.00%
+
+#Instruction: example of using least JOIN to do most things
