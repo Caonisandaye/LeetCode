a200f656d868b9c732d9a1ad1611d4a92fbd48b9
Jianze Zhou <jnz2@georgetown.edu>
2021-09-19 20:30:20 -0400

    Update 0000(template).sql

--- 0000(template).sql
+++ 0000(template).sql
@@ -3,6 +3,6 @@
 #Solution
 SELECT sample_column FROM sample_table
                 
-#Result Runtime: __ ms / __%; Memory Usage: __ MB / __%
+#Result Runtime: __ ms / __%; Memory Usage: 0 MB / 100.00%
 
 #Instruction: XXXXXX
