5d212f348c12776c307f0e575b71f70d6c775e04
Jianze Zhou <jnz2@georgetown.edu>
2021-10-05 17:26:55 -0400

    Create 0596.sql

---
+++ 0596.sql
@@ -0,0 +1,8 @@
+##LC 596. Classes More Than 5 Students
+
+#Solution
+SELECT sample_column FROM sample_table
+                
+#Result Runtime: 223 ms / 99.74%; Memory Usage: 0 MB / 100.00%
+
+#Instruction: simple grouping and HAVING clause
