39adfb1d65cb47588ca91e448ec4ef2dc797b68e
Jianze Zhou <jnz2@georgetown.edu>
2021-10-06 03:42:09 -0400

    Create 1853.sql

---
+++ 1853.sql
@@ -0,0 +1,9 @@
+##LC 1853. Convert Date Format
+
+#Solution
+SELECT CONCAT(DAYNAME(day), ", ", MONTHNAME(day), " ", DAY(day), ", ", YEAR(day)) day
+FROM Days
+                
+#Result Runtime: 435 ms / 78.14%; Memory Usage: 0 MB / 100.00%
+
+#Instruction: remember some import date functions
