96260b947f83bdf4023167db14271c878b3c9812
Jianze Zhou <jnz2@georgetown.edu>
2021-10-06 18:22:58 -0400

    Update 1378.sql

--- 1378.sql
+++ 1378.sql
@@ -5,6 +5,6 @@ SELECT B.unique_id, A.name
 FROM Employees A
 LEFT JOIN EmployeeUNI B USING(id)
                 
-#Result Runtime: 890 ms / 85.08%; Memory Usage: 0 MB / 100.00%
+#Result Runtime: 887 ms / 85.92%; Memory Usage: 0 MB / 100.00%
 
 #Instruction: simple LEFT JOIN
