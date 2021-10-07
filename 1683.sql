1fce78beda308c779892cb3c52d245af1c3a9033
Jianze Zhou <jnz2@georgetown.edu>
2021-10-07 01:35:40 -0400

    Create 1683.sql

---
+++ 1683.sql
@@ -0,0 +1,8 @@
+##LC 1683. Invalid Tweets
+
+#Solution
+SELECT tweet_id FROM tweets WHERE LENGTH(content) > 15
+                
+#Result Runtime: 474 ms / 93.45%; Memory Usage: 0 MB / 100.00%
+
+#Instruction: super easy one
