a8ffdd338e8bb69b016ecf47d75378479a19b031
Jianze Zhou <jnz2@georgetown.edu>
2025-01-05 20:34:26 -0600

    Create 0026.py

---
+++ 0026.py
@@ -0,0 +1,17 @@
+##LC 0. py file template
+
+#Solution
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        lst = 0
+        for i, n in enumerate(nums[1:], 1):
+            if n != nums[lst]:
+                lst += 1
+                nums[lst] = n
+        lst += 1
+        nums[lst:] = ["_"] * (len(nums) - lst)
+        return lst
+                
+#Result Runtime: 0 ms / 100.00%; Memory Usage: 19.16 MB / 10.38%
+
+#Instruction: Keep checking current number against last seen non-duplicate number and replace in-place to avoid a second pass
