fabd18765925f954758496bba85f721c86c46ebb
Jianze Zhou <jnz2@georgetown.edu>
2025-01-05 20:21:44 -0600

    Create 0021.py

---
+++ 0021.py
@@ -0,0 +1,24 @@
+##LC 21. Merge Two Sorted Lists
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        cur = head = ListNode(-101, None)
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next, list1 = list1, list1.next
+            else:
+                cur.next, list2 = list2, list2.next
+            cur = cur.next
+        cur.next = list1 if list1 else list2
+        return head.next
+
+#Result Runtime: 0 ms / 100.00%; Memory Usage: 17.96 MB / 5.62%
+
+#Instruction: Merge the two list to save space (not sure why memory usage is still bad)
