ea22c0e1d1883a5c86d8654a3f37c019bba54e72
Jianze Zhou <jnz2@georgetown.edu>
2025-01-05 19:27:04 -0600

    Create 0009.py

---
+++ 0009.py
@@ -0,0 +1,19 @@
+##LC 0. py file template
+
+#Solution
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        digits = []
+        while x:
+            x, d = divmod(x, 10)
+            digits.append(d)
+        for i in range(len(digits) // 2):
+            if digits[i] != digits[-i - 1]:
+                return False
+        return True
+                
+#Result Runtime: 19 ms / 9.67%; Memory Usage: 17.89 MB / 12.26%
+
+#Instruction: Use divmod to construct a digit list instead of converting th integer into a string, which may sacrifice some time complexity.
