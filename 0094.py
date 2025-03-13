c239f53f0092dab47167c63f5e0b29d76fb92696
Jianze Zhou <jnz2@georgetown.edu>
2025-03-13 01:19:13 -0500

    Update 0094.py

--- 0094.py
+++ 0094.py
@@ -11,7 +11,11 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+        return (
+            self.inorderTraversal(root.left)
+            + [root.val]
+            + self.inorderTraversal(root.right)
+        )
                 
 #Result Runtime: 0 ms / 100.00%; Memory Usage: 17.59 MB / 97.29%
 
@@ -19,12 +23,14 @@ class Solution:
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
+
         def inorderTraversalAndPrint(node):
             if not node:
                 return
             inorderTraversalAndPrint(node.left)
             ans.append(node.val)
             inorderTraversalAndPrint(node.right)
+
         inorderTraversalAndPrint(root)
         return ans
 
