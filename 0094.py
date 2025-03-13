c27c05f8397ea5c183d31c50965514c427724674
Jianze Zhou <jnz2@georgetown.edu>
2025-03-13 01:05:02 -0500

    Create 0094.py

---
+++ 0094.py
@@ -0,0 +1,33 @@
+##LC 94. Binary Tree Inorder Traversal
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#Solution1
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+                
+#Result Runtime: 0 ms / 100.00%; Memory Usage: 17.59 MB / 97.29%
+
+#Solution2
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        def inorderTraversalAndPrint(node):
+            if not node:
+                return
+            inorderTraversalAndPrint(node.left)
+            ans.append(node.val)
+            inorderTraversalAndPrint(node.right)
+        inorderTraversalAndPrint(root)
+        return ans
+
+#Result Runtime: 0 ms / 100.00%; Memory Usage: 17.77 MB / 54.80%
+
+#Instruction: classic recurssion, second solution can avoid too many list operations.
