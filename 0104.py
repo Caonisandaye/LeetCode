b2163f1c0be04cd45e3008620bbeefa15d5b9e49
Jianze Zhou <jnz2@georgetown.edu>
2025-03-13 01:23:43 -0500

    Create 0104.py

---
+++ 0104.py
@@ -0,0 +1,18 @@
+##LC 104. Maximum Depth of Binary Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#Solution
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+                
+#Result Runtime: 3 ms / 30.72%; Memory Usage: 18.99 MB / 78.97%
+
+#Instruction: Classic recurssion, also, all in the answer.
