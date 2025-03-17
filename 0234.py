095c9e12e04552a7d9ec6ca58d8e1c3ea1263ad7
Jianze Zhou <jnz2@georgetown.edu>
2025-03-16 22:28:25 -0500

    Create 0234.py

---
+++ 0234.py
@@ -0,0 +1,9 @@
+##LC 234. Palindrome Linked List
+
+#Solution
+class Solution(object):
+    def sampleMethod(self, arg: None) -> None:
+                
+#Result Runtime: __ ms / __%; Memory Usage: __ MB / __%
+
+#Instruction: XXXXXX
